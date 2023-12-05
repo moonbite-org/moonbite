a37c041e1238f1be48ad1b2d4de163d4dbd06c51
Muhammed Ali CAN <tmwwdw@gmail.com>
2023-12-05 15:43:56 +0300

    feat: add vm module

--- formatter/main.go
+++ formatter/main.go
@@ -9,20 +9,20 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		os.Stdout.WriteString("no file provided.")
+		os.Stderr.WriteString("no file provided.")
 		os.Exit(1)
 	}
 
 	input, err := os.ReadFile(os.Args[1])
 
 	if err != nil {
-		os.Stdout.WriteString(err.Error())
+		os.Stderr.WriteString(err.Error())
 		os.Exit(1)
 	}
 
 	ast, p_err := parser.Parse(input, os.Args[1])
 	if p_err.Exists {
-		os.Stdout.WriteString(p_err.String())
+		os.Stderr.WriteString(p_err.String())
 		os.Exit(1)
 	}
 
